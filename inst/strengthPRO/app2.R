9050db6c3569e93aa49ae74bb61ec748c187a3d8
Mladen Jovanović <coach.mladen.jovanovic@gmail.com>
2022-01-11 20:56:06 +0100

    Progression tables finished

--- inst/strengthPRO/app2.R
+++ inst/strengthPRO/app2.R
@@ -189,7 +189,10 @@ ui <- dashboardPage(
             tabPanel(
               title = "Reps-Max Table",
               fluidPage(
-                dataTableOutput("single_athlete_reps_max_table")
+                dataTableOutput("single_athlete_reps_max_table"),
+                br(), br(),
+                HTML("<b>Note:</b> Reps-max table representes estimated %1RM for a target repetitions and
+                     selected adjustment, given the model used")
               )
             ), # Reps Max Table Panel
             # Adjustments
@@ -210,7 +213,11 @@ ui <- dashboardPage(
                   4,
                   h5("Extensive variant"),
                   dataTableOutput("single_athlete_progression_table_extensive_adjustment")
-                )
+                ),
+                br(), br(),
+                HTML("<b>Note:</b> Negative numbers in the columns represent progression steps
+                     as defined by a selected progression table model. Numbers in cells represent
+                     adjustments to be utilized for the calculus of the %1RM")
               )
             ), # Adjustments
             # Progression %1RMs
@@ -231,7 +238,11 @@ ui <- dashboardPage(
                   4,
                   h5("Extensive variant"),
                   dataTableOutput("single_athlete_progression_table_extensive_perc1RM")
-                )
+                ),
+                br(), br(),
+                HTML("<b>Note:</b> Negative numbers in the columns represent progression steps
+                     as defined by a selected progression table model. Numbers in cells represent
+                     estimated %1RM to be used")
               )
             ) # Progression %1RMs
           ), # Progression Tab box
@@ -925,11 +936,159 @@ server <- function(input, output) {
       prediction_equation <- sprintf(print_text, eq_parameter_value)
     }
 
+    # Function factory for the reps-max equation
+    get_max_perc_1RM_RIR_func <- switch(model_used,
+      "Epley's" = function(...) get_max_perc_1RM_k(..., k = parameter_value),
+      "Modified Epley's" = function(...) get_max_perc_1RM_kmod(..., kmod = parameter_value),
+      "Linear" =  function(...) get_max_perc_1RM_klin(..., klin = parameter_value)
+    )
 
-    # Reutnr object
+    get_max_perc_1RM_relInt_func <- switch(model_used,
+      "Epley's" = function(...) get_max_perc_1RM_k_relInt(..., k = parameter_value),
+      "Modified Epley's" = function(...) get_max_perc_1RM_kmod_relInt(..., kmod = parameter_value),
+      "Linear" =  function(...) get_max_perc_1RM_klin_relInt(..., klin = parameter_value)
+    )
+
+    get_max_perc_1RM_percMR_func <- switch(model_used,
+      "Epley's" = function(...) get_max_perc_1RM_k_percMR(..., k = parameter_value),
+      "Modified Epley's" = function(...) get_max_perc_1RM_kmod_percMR(..., kmod = parameter_value),
+      "Linear" =  function(...) get_max_perc_1RM_klin_percMR(..., klin = parameter_value)
+    )
+
+    get_max_perc_1RM_DI_func <- switch(model_used,
+      "Epley's" = function(..., adjustment) get_max_perc_1RM_k(..., k = parameter_value) + adjustment,
+      "Modified Epley's" = function(..., adjustment) get_max_perc_1RM_kmod(..., kmod = parameter_value) + adjustment,
+      "Linear" =  function(..., adjustment) get_max_perc_1RM_klin(..., klin = parameter_value) + adjustment
+    )
+
+    # Function factory for the progression table
+    progression_table_func <- switch(progression_table,
+      "Deducted Intensity 2.5%" = function(...) perc_drop_fixed_25(..., func_max_perc_1RM = get_max_perc_1RM_RIR_func),
+      "Deducted Intensity 5%" = function(...) perc_drop_fixed_5(..., func_max_perc_1RM = get_max_perc_1RM_RIR_func),
+      "Relative Intensity" = function(...) relInt(..., func_max_perc_1RM = get_max_perc_1RM_relInt_func),
+      "Perc Drop" = function(...) perc_drop(..., func_max_perc_1RM = get_max_perc_1RM_RIR_func),
+      "RIR 1" = function(...) RIR_increment_fixed_1(..., func_max_perc_1RM = get_max_perc_1RM_RIR_func),
+      "RIR 2" = function(...) RIR_increment_fixed_2(..., func_max_perc_1RM = get_max_perc_1RM_RIR_func),
+      "RIR Inc" = function(...) RIR_increment(..., func_max_perc_1RM = get_max_perc_1RM_RIR_func),
+      "%MR Step Const" = function(...) percMR_step_const(..., func_max_perc_1RM = get_max_perc_1RM_percMR_func),
+      "%MR Step Var" = function(...) percMR_step_var(..., func_max_perc_1RM = get_max_perc_1RM_percMR_func)
+    )
+
+    adjustment_seq <- switch(progression_table,
+      "Deducted Intensity 2.5%" = seq(0, -30, by = -2.5) / 100,
+      "Deducted Intensity 5%" = seq(0, -60, by = -5) / 100,
+      "Relative Intensity" = seq(100, 60, by = -2.5) / 100,
+      "Perc Drop" = seq(0, -30, by = -2.5) / 100,
+      "RIR 1" = seq(0, 15, by = 1),
+      "RIR 2" = seq(0, 15, by = 1),
+      "RIR Inc" = seq(0, 15, by = 1),
+      "%MR Step Const" = seq(100, 30, by = -5) / 100,
+      "%MR Step Var" = seq(100, 30, by = -5) / 100
+    )
+
+    adjustment_values <- switch(progression_table,
+      "Deducted Intensity 2.5%" = paste0(seq(0, -30, by = -2.5), "%"),
+      "Deducted Intensity 5%" = paste0(seq(0, -60, by = -5), "%"),
+      "Relative Intensity" = paste0(seq(100, 60, by = -2.5), "%"),
+      "Perc Drop" = paste0(seq(0, -30, by = -2.5), "%"),
+      "RIR 1" = paste0(seq(0, 15, by = 1), "RIR"),
+      "RIR 2" = paste0(seq(0, 15, by = 1), "RIR"),
+      "RIR Inc" = paste0(seq(0, 15, by = 1), "RIR"),
+      "%MR Step Const" = paste0(seq(100, 30, by = -5), "%MR"),
+      "%MR Step Var" = paste0(seq(100, 30, by = -5), "%MR")
+    )
+
+    get_max_perc_1RM_func <- switch(progression_table,
+      "Deducted Intensity 2.5%" = get_max_perc_1RM_DI_func,
+      "Deducted Intensity 5%" = get_max_perc_1RM_DI_func,
+      "Relative Intensity" = get_max_perc_1RM_relInt_func,
+      "Perc Drop" = get_max_perc_1RM_DI_func,
+      "RIR 1" = get_max_perc_1RM_RIR_func,
+      "RIR 2" = get_max_perc_1RM_RIR_func,
+      "RIR Inc" = get_max_perc_1RM_RIR_func,
+      "%MR Step Const" = get_max_perc_1RM_percMR_func,
+      "%MR Step Var" = get_max_perc_1RM_percMR_func
+    )
+
+    reps_max_tbl <- expand_grid(
+      Reps = seq(1, 12),
+      data.frame(adjustment = adjustment_seq, adjustment_val = adjustment_values)
+    ) %>%
+      mutate(
+        perc_1RM = round(
+          100 * get_max_perc_1RM_func(
+            max_reps = Reps,
+            adjustment = adjustment,
+            type = tolower(progression_table_type)
+          ),
+          1
+        )
+      ) %>%
+      pivot_wider(id_cols = Reps, names_from = adjustment_val, values_from = perc_1RM)
+
+    # Progression table
+    progression_tbl_long <- generate_progression_table(
+      progression_table = progression_table_func,
+      type = tolower(progression_table_type),
+      reps = 1:12,
+      step = c(-3, -2, -1, 0)
+    ) %>%
+      mutate(
+        perc_1RM = round(100 * perc_1RM, 1),
+        adjustment = switch(progression_table,
+          "Deducted Intensity 2.5%" = round(adjustment * 100, 1),
+          "Deducted Intensity 5%" = round(adjustment * 100, 1),
+          "Relative Intensity" = round(adjustment * 100, 1),
+          "Perc Drop" = round(adjustment * 100, 1),
+          "RIR 1" = round(adjustment, 1),
+          "RIR 2" = round(adjustment, 1),
+          "RIR Inc" = round(adjustment, 1),
+          "%MR Step Const" = round(adjustment * 100, 1),
+          "%MR Step Var" = round(adjustment * 100, 1)
+        ),
+        step = step # length(unique(step)) + step
+      ) %>%
+      rename(Step = step, Reps = reps)
+
+    # Convert to wide
+    # %1RM
+    progression_table_intensive_perc1RM <- progression_tbl_long %>%
+      filter(volume == "intensive") %>%
+      pivot_wider(id_cols = Reps, names_from = Step, values_from = perc_1RM)
+
+    progression_table_normal_perc1RM <- progression_tbl_long %>%
+      filter(volume == "normal") %>%
+      pivot_wider(id_cols = Reps, names_from = Step, values_from = perc_1RM)
+
+    progression_table_extensive_perc1RM <- progression_tbl_long %>%
+      filter(volume == "extensive") %>%
+      pivot_wider(id_cols = Reps, names_from = Step, values_from = perc_1RM)
+
+    # Adjustments
+    progression_table_intensive_adjustment <- progression_tbl_long %>%
+      filter(volume == "intensive") %>%
+      pivot_wider(id_cols = Reps, names_from = Step, values_from = adjustment)
+
+    progression_table_normal_adjustment <- progression_tbl_long %>%
+      filter(volume == "normal") %>%
+      pivot_wider(id_cols = Reps, names_from = Step, values_from = adjustment)
+
+    progression_table_extensive_adjustment <- progression_tbl_long %>%
+      filter(volume == "extensive") %>%
+      pivot_wider(id_cols = Reps, names_from = Step, values_from = adjustment)
+
+    # Return object
     list(
       parameter_value = parameter_value,
-      prediction_equation = prediction_equation
+      prediction_equation = prediction_equation,
+      reps_max_tbl = reps_max_tbl,
+      progression_tbl_long =  progression_tbl_long,
+      progression_table_intensive_perc1RM = progression_table_intensive_perc1RM,
+      progression_table_normal_perc1RM = progression_table_normal_perc1RM,
+      progression_table_extensive_perc1RM = progression_table_extensive_perc1RM,
+      progression_table_intensive_adjustment = progression_table_intensive_adjustment,
+      progression_table_normal_adjustment = progression_table_normal_adjustment,
+      progression_table_extensive_adjustment = progression_table_extensive_adjustment
     )
   })
 
@@ -937,6 +1096,123 @@ server <- function(input, output) {
   output$single_athlete_prediction_equation <- renderText({
     paste0("<b>", single_athlete_progression_table()$prediction_equation, "</b>")
   })
+
+  # Reps-max table
+  output$single_athlete_reps_max_table <- renderDT({
+    reps_max_tbl <- single_athlete_progression_table()$reps_max_tbl
+
+    DT::datatable(
+      reps_max_tbl,
+      rownames = FALSE,
+      selection = "none",
+      editable = FALSE,
+      options = list(
+        searching = FALSE,
+        ordering = FALSE,
+        paging = FALSE,
+        info = FALSE
+      )
+    )
+  })
+
+  # Progression Table adjustments
+  output$single_athlete_progression_table_intensive_adjustment <- renderDT({
+
+    DT::datatable(
+      single_athlete_progression_table()$progression_table_intensive_adjustment,
+      rownames = FALSE,
+      selection = "none",
+      editable = FALSE,
+      options = list(
+        searching = FALSE,
+        ordering = FALSE,
+        paging = FALSE,
+        info = FALSE
+      )
+    )
+  })
+
+  output$single_athlete_progression_table_normal_adjustment <- renderDT({
+
+    DT::datatable(
+      single_athlete_progression_table()$progression_table_normal_adjustment,
+      rownames = FALSE,
+      selection = "none",
+      editable = FALSE,
+      options = list(
+        searching = FALSE,
+        ordering = FALSE,
+        paging = FALSE,
+        info = FALSE
+      )
+    )
+  })
+
+  output$single_athlete_progression_table_extensive_adjustment <- renderDT({
+
+    DT::datatable(
+      single_athlete_progression_table()$progression_table_extensive_adjustment,
+      rownames = FALSE,
+      selection = "none",
+      editable = FALSE,
+      options = list(
+        searching = FALSE,
+        ordering = FALSE,
+        paging = FALSE,
+        info = FALSE
+      )
+    )
+  })
+
+  # Progression Table %1RM
+  output$single_athlete_progression_table_intensive_perc1RM <- renderDT({
+
+    DT::datatable(
+      single_athlete_progression_table()$progression_table_intensive_perc1RM,
+      rownames = FALSE,
+      selection = "none",
+      editable = FALSE,
+      options = list(
+        searching = FALSE,
+        ordering = FALSE,
+        paging = FALSE,
+        info = FALSE
+      )
+    )
+  })
+
+  output$single_athlete_progression_table_normal_perc1RM <- renderDT({
+
+    DT::datatable(
+      single_athlete_progression_table()$progression_table_normal_perc1RM,
+      rownames = FALSE,
+      selection = "none",
+      editable = FALSE,
+      options = list(
+        searching = FALSE,
+        ordering = FALSE,
+        paging = FALSE,
+        info = FALSE
+      )
+    )
+  })
+
+  output$single_athlete_progression_table_extensive_perc1RM <- renderDT({
+
+    DT::datatable(
+      single_athlete_progression_table()$progression_table_extensive_perc1RM,
+      rownames = FALSE,
+      selection = "none",
+      editable = FALSE,
+      options = list(
+        searching = FALSE,
+        ordering = FALSE,
+        paging = FALSE,
+        info = FALSE
+      )
+    )
+  })
+
 }
 
 
