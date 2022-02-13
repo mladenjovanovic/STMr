8fc8e6861924216f23224da0bcd08aac63dd1c4c
Mladen Jovanović <coach.mladen.jovanovic@gmail.com>
2022-02-13 20:55:45 +0100

    Reorginazied quantile estimation functions

--- R/estimate-quantile-functions.R
+++ R/estimate-quantile-functions.R
@@ -4,6 +4,7 @@
 #' target variable is the reps performed, while the predictors is the \code{perc_1RM} or
 #' \code{weight}. To reverse this, use the \code{reverse = TRUE} argument
 #'
+#' @param perc_1RM %1RM
 #' @param weight Weight used
 #' @param reps Number of repetitions done
 #' @param eRIR Subjective estimation of reps-in-reserve (eRIR)
@@ -16,6 +17,54 @@
 #' @name estimate_functions_quantile
 NULL
 
+#' @describeIn estimate_functions_quantile Estimate the parameter \code{k} in the Epley's equation
+#' @export
+#' @examples
+#' # ---------------------------------------------------------
+#' # Epley's model
+#' m1 <- estimate_k_quantile(
+#'   perc_1RM = c(0.7, 0.8, 0.9),
+#'   reps = c(10, 5, 3)
+#' )
+#'
+#' coef(m1)
+estimate_k_quantile <- function(perc_1RM,
+                                    reps,
+                                    eRIR = 0,
+                                    tau = 0.5,
+                                    reverse = FALSE,
+                                    control = quantreg::nlrq.control(maxiter = 10^4, InitialStepSize = 0),
+                                    ...) {
+
+  df <- data.frame(perc_1RM = perc_1RM, reps = reps, eRIR = eRIR) %>%
+    dplyr::mutate(
+      nRM = reps + eRIR
+    )
+
+  if (reverse == FALSE) {
+    m1 <- quantreg::nlrq(
+      nRM ~ (1 - perc_1RM) / (k * perc_1RM),
+      data =  df,
+      start = list(k = 1),
+      tau = tau,
+      control = control,
+      ...
+    )
+  } else {
+    m1 <- quantreg::nlrq(
+      perc_1RM ~ 1 / (k * nRM + 1),
+      data =  df,
+      start = list(k = 1),
+      tau = tau,
+      control = control,
+      ...
+    )
+  }
+
+  m1
+}
+
+
 
 #' @describeIn estimate_functions_quantile Estimate the parameter \code{k} in the Epley's equation, as well as
 #'     \code{1RM}. This is a novel estimation function that uses the absolute weights
@@ -23,13 +72,13 @@ NULL
 #' @examples
 #' # ---------------------------------------------------------
 #' # Epley's model that also estimates 1RM
-#' m1 <- estimate_k_quantile(
+#' m1 <- estimate_k_1RM_quantile(
 #'   weight = c(70, 110, 140),
 #'   reps = c(10, 5, 3)
 #' )
 #'
 #' coef(m1)
-estimate_k_quantile <- function(weight,
+estimate_k_1RM_quantile <- function(weight,
                                 reps,
                                 eRIR = 0,
                                 tau = 0.5,
@@ -65,19 +114,66 @@ estimate_k_quantile <- function(weight,
   m1
 }
 
+#' @describeIn estimate_functions_quantile Estimate the parameter \code{kmod} in the modified Epley's equation
+#' @export
+#' @examples
+#' # ---------------------------------------------------------
+#' # Modified Epley's model
+#' m1 <- estimate_kmod_quantile(
+#'   perc_1RM = c(0.7, 0.8, 0.9),
+#'   reps = c(10, 5, 3)
+#' )
+#'
+#' coef(m1)
+estimate_kmod_quantile <- function(perc_1RM,
+                                       reps,
+                                       eRIR = 0,
+                                       tau = 0.5,
+                                       reverse = FALSE,
+                                       control = quantreg::nlrq.control(maxiter = 10^4, InitialStepSize = 0),
+                                       ...) {
+
+  df <- data.frame(perc_1RM = perc_1RM, reps = reps, eRIR = eRIR) %>%
+    dplyr::mutate(
+      nRM = reps + eRIR
+    )
+
+  if (reverse == FALSE) {
+    m1 <- quantreg::nlrq(
+      nRM ~ ((kmod - 1) * perc_1RM + 1) / (kmod * perc_1RM),
+      data =  df,
+      start = list(kmod = 1),
+      tau = tau,
+      control = control,
+      ...
+    )
+  } else {
+    m1 <- quantreg::nlrq(
+      perc_1RM ~ 1 / (kmod * (nRM - 1) + 1),
+      data =  df,
+      start = list(kmod = 1),
+      tau = tau,
+      control = control,
+      ...
+    )
+  }
+
+  m1
+}
+
 #' @describeIn estimate_functions_quantile Estimate the parameter \code{kmod} in the modified Epley's equation, as well as
 #'     \code{1RM}. This is a novel estimation function that uses the absolute weights
 #' @export
 #' @examples
 #' # ---------------------------------------------------------
 #' # Modified Epley's model that also estimates 1RM
-#' m1 <- estimate_kmod_quantile(
+#' m1 <- estimate_kmod_1RM_quantile(
 #'   weight = c(70, 110, 140),
 #'   reps = c(10, 5, 3)
 #' )
 #'
 #' coef(m1)
-estimate_kmod_quantile <- function(weight,
+estimate_kmod_1RM_quantile <- function(weight,
                                 reps,
                                 eRIR = 0,
                                 tau = 0.5,
@@ -113,19 +209,66 @@ estimate_kmod_quantile <- function(weight,
   m1
 }
 
+#' @describeIn estimate_functions_quantile Estimate the parameter \code{klin} in the Linear/Brzycki equation
+#' @export
+#' @examples
+#' # ---------------------------------------------------------
+#' # Linear/Brzycki model
+#' m1 <- estimate_klin_quantile(
+#'   perc_1RM = c(0.7, 0.8, 0.9),
+#'   reps = c(10, 5, 3)
+#' )
+#'
+#' coef(m1)
+estimate_klin_quantile <- function(perc_1RM,
+                                       reps,
+                                       eRIR = 0,
+                                       tau = 0.5,
+                                       reverse = FALSE,
+                                       control = quantreg::nlrq.control(maxiter = 10^4, InitialStepSize = 0),
+                                       ...) {
+
+  df <- data.frame(perc_1RM = perc_1RM, reps = reps, eRIR = eRIR) %>%
+    dplyr::mutate(
+      nRM = reps + eRIR
+    )
+
+  if (reverse == FALSE) {
+    m1 <- quantreg::nlrq(
+      nRM ~ (1 - perc_1RM) * klin + 1,
+      data =  df,
+      start = list(klin = 1),
+      tau = tau,
+      control = control,
+      ...
+    )
+  } else {
+    m1 <- quantreg::nlrq(
+      perc_1RM ~ (klin - nRM + 1) / klin,
+      data =  df,
+      start = list(klin = 1),
+      tau = tau,
+      control = control,
+      ...
+    )
+  }
+
+  m1
+}
+
 #' @describeIn estimate_functions_quantile Estimate the parameter \code{klin} in the Linear/Brzycki equation, as well as
 #'     \code{1RM}. This is a novel estimation function that uses the absolute weights
 #' @export
 #' @examples
 #' # ---------------------------------------------------------
 #' # Linear/Brzycki model thal also estimates 1RM
-#' m1 <- estimate_klin_quantile(
+#' m1 <- estimate_klin_1RM_quantile(
 #'   weight = c(70, 110, 140),
 #'   reps = c(10, 5, 3)
 #' )
 #'
 #' coef(m1)
-estimate_klin_quantile <- function(weight,
+estimate_klin_1RM_quantile <- function(weight,
                                    reps,
                                    eRIR = 0,
                                    tau = 0.5,
