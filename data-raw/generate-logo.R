b7a2d47e8713b99461769d5a9474b008f72a34de
Mladen Jovanović <coach.mladen.jovanovic@gmail.com>
2022-03-11 09:09:38 +0100

    Location of the logo output

--- data-raw/generate-logo.R
+++ data-raw/generate-logo.R
@@ -7,5 +7,5 @@ s <- sticker(imgurl,
   s_width = 0.95, s_x = 1, s_y = 1.15,
   h_fill = rgb(82, 57, 57, maxColorValue = 255),
   h_color = rgb(226, 110, 82, maxColorValue = 255),
-  filename = "inst/figures/logo.png"
+  filename = "man/figures/logo.png"
 )
